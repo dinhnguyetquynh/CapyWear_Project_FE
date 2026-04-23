ed56207b45e7506b5296482fd79605e5be11d685
dinhnguyetquynh <dquynhd202@gmail.com>
2026-04-23 11:13:51 +0700

    fix history page 2

---
+++ app/(shop)/history/history-container.tsx
@@ -0,0 +1,20 @@
+//container
+'use client'
+import { Suspense, useEffect, useState } from "react";
+import { useOrderHistory } from "./useOrderHistory";
+import HistoryOrder from "./history-order";
+
+const HistoryOrderContainer = () => {
+    const { listOrderHistory, isLoading, error } = useOrderHistory();
+
+    if (isLoading) return <div>Đang tải dữ liệu...</div>;
+    if (error) return <div>{error}</div>;
+
+    return (
+        <div>
+            <HistoryOrder orders={listOrderHistory}/>
+        </div>
+    );
+};
+
+export default HistoryOrderContainer;
--- app/(shop)/history/page.tsx
+++ app/(shop)/history/page.tsx
@@ -1,26 +1,10 @@
-//container
-'use client'
-import { Suspense, useEffect, useState } from "react";
-import { useOrderHistory } from "./useOrderHistory";
-import HistoryOrder from "./history-order";
-
-const HistoryOrderContainer = () => {
-    const { listOrderHistory, isLoading, error } = useOrderHistory();
-
-    if (isLoading) return <div>Đang tải dữ liệu...</div>;
-    if (error) return <div>{error}</div>;
-
-    return (
-        <div>
-            <HistoryOrder orders={listOrderHistory}/>
-        </div>
-    );
-};
+import { Suspense } from "react";
+import HistoryOrderContainer from "./history-container";
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <HistoryOrderContainer />
+      <HistoryOrderContainer/>
     </Suspense>
   );
 }
