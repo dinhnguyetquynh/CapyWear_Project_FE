bdc31db348b2c464ddddabcc5d75a0c68788f02f
dinhnguyetquynh <dquynhd202@gmail.com>
2026-04-23 10:11:01 +0700

    fix cart page

--- app/(shop)/cart/page.tsx
+++ app/(shop)/cart/page.tsx
@@ -5,7 +5,7 @@ import { getCartList } from "@/service/cart.service";
 import { getServerSession } from "next-auth";
 import { getSession, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 export default function ListCartDetail(){
     const router = useRouter();
 
@@ -63,45 +63,48 @@ useEffect(() => {
     if (loading) return <div>Đang tải giỏ hàng...</div>;
     return(
         <main className="max-w-5xl mx-auto p-6">
-        <h1 className="text-2xl font-bold mb-8 text-gray-800">Giỏ hàng của bạn</h1>
+        <Suspense>
+            <h1 className="text-2xl font-bold mb-8 text-gray-800">Giỏ hàng của bạn</h1>
 
-        {cartItems.length === 0 ? (
-            <div className="py-20 text-center border rounded-lg bg-gray-50">
-            <p className="text-gray-500">Giỏ hàng của bạn đang trống.</p>
-            </div>
-        ) : (
-            <div className="grid grid-cols-1 gap-6">
-            {/* 4. Duyệt danh sách và hiển thị */}
-            {cartItems.map((item: any) => (
-                <CartDetail 
-                    key={item.id} 
-                    cd={item} 
-                    accessToken={accessToken}
-                    isSelected={selectedIds.includes(item.id)}
-                    onSelect={() => toggleSelect(item.id)}
-                    onUpdateQuantity={(newQty) => {           
-                    const newCart = cartItems.map(c => 
-                        c.id === item.id ? {...c, quantity: newQty} : c
-                    );
-                    setCartItems(newCart); 
-                    }}
-                    onDeleteSuccess ={handleDeleteFromState}
-                />
-            ))}
-
-            <div className="mt-6 flex flex-col items-end border-t pt-6">
-                        <div className="text-xl mb-4">
-                            Tổng cộng ({selectedIds.length} món): 
-                            <span className="text-red-600 font-bold ml-2">
-                                {totalPrice.toLocaleString()} VNĐ
-                            </span>
-                        </div>
-                        <button className="bg-blue-600 text-white px-8 py-3 rounded-lg font-semibold hover:bg-blue-700 transition" onClick={handleCheckout}>
-                            Đặt hàng
-                        </button>
+                {cartItems.length === 0 ? (
+                    <div className="py-20 text-center border rounded-lg bg-gray-50">
+                    <p className="text-gray-500">Giỏ hàng của bạn đang trống.</p>
                     </div>
-            </div>  
-        )}
+                ) : (
+                    <div className="grid grid-cols-1 gap-6">
+                    {/* 4. Duyệt danh sách và hiển thị */}
+                    {cartItems.map((item: any) => (
+                        <CartDetail 
+                            key={item.id} 
+                            cd={item} 
+                            accessToken={accessToken}
+                            isSelected={selectedIds.includes(item.id)}
+                            onSelect={() => toggleSelect(item.id)}
+                            onUpdateQuantity={(newQty) => {           
+                            const newCart = cartItems.map(c => 
+                                c.id === item.id ? {...c, quantity: newQty} : c
+                            );
+                            setCartItems(newCart); 
+                            }}
+                            onDeleteSuccess ={handleDeleteFromState}
+                        />
+                    ))}
+
+                    <div className="mt-6 flex flex-col items-end border-t pt-6">
+                                <div className="text-xl mb-4">
+                                    Tổng cộng ({selectedIds.length} món): 
+                                    <span className="text-red-600 font-bold ml-2">
+                                        {totalPrice.toLocaleString()} VNĐ
+                                    </span>
+                                </div>
+                                <button className="bg-blue-600 text-white px-8 py-3 rounded-lg font-semibold hover:bg-blue-700 transition" onClick={handleCheckout}>
+                                    Đặt hàng
+                                </button>
+                            </div>
+                    </div>  
+                )}
+        </Suspense>
+        
         </main>
     );
 
