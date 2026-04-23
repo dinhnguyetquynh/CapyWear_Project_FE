1d25597499587bce9acd283989b8d5e8a446ab0a
dinhnguyetquynh <dquynhd202@gmail.com>
2026-04-23 11:02:24 +0700

    fix history page 2

--- app/(shop)/history/page.tsx
+++ app/(shop)/history/page.tsx
@@ -1,22 +1,26 @@
 //container
 'use client'
-import { getOrderHistory, OrderRespone } from "@/service/order.service";
-import { useSession } from "next-auth/react";
 import { Suspense, useEffect, useState } from "react";
 import { useOrderHistory } from "./useOrderHistory";
 import HistoryOrder from "./history-order";
 
-export default function HistoryOrderContainer(){
+const HistoryOrderContainer = () => {
     const { listOrderHistory, isLoading, error } = useOrderHistory();
 
     if (isLoading) return <div>Đang tải dữ liệu...</div>;
     if (error) return <div>{error}</div>;
 
-    return(
+    return (
         <div>
-            <Suspense>
-                 <HistoryOrder orders={listOrderHistory}/>
-            </Suspense>
+            <HistoryOrder orders={listOrderHistory}/>
         </div>
     );
+};
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <HistoryOrderContainer />
+    </Suspense>
+  );
 }
