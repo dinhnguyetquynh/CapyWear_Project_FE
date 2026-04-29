8d56fef39cadf6b6ad7f609430d2c42d6ed5b80e
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-04-29 14:07:26 +0700

    test url api

--- service/item.service.ts
+++ service/item.service.ts
@@ -106,9 +106,12 @@ export const getItems = async (
 
   // 3. Gọi fetch với query string đã được tạo tự động
   console.log('API URL :'+API_URL);
-  const res = await fetch(`${API_FE}/api/v1/items?${params.toString()}`, {
-    next: { revalidate: 60 }, 
-  });
+  // const res = await fetch(`${API_FE}/api/v1/items?${params.toString()}`, {
+  //   next: { revalidate: 60 }, 
+  // });
+  const res = await fetch(`${API_URL}/api/item/range-price?${params.toString()}`,{
+    next:{ revalidate: 60 },
+  })
 
   if (!res.ok) {
     throw new Error("Không thể lấy danh sách sản phẩm");
