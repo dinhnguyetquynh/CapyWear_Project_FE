c45fca3258b2d3520666a95a14d01e326ea4d4b1
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-04-29 12:11:45 +0700

    test homepage

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -2,5 +2,10 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 export default function RootPage() {
   // Tự động chuyển hướng sang trang sản phẩm
-  redirect("/products");
+  // redirect("/products");
+  return(
+    <div>
+      <p>HELLO HOME PAGE</p>
+    </div>
+  )
 }
--- app/api/v1/items/[id]/route.ts
+++ app/api/v1/items/[id]/route.ts
@@ -1,22 +1,5 @@
 import { NextRequest } from "next/server";
 
-// export async function GET(
-//   req: NextRequest,
-//   context: { params: Promise<{ id: string }> }
-// ) {
-//   console.log("CALL API PROXY:");
-//   const { id } = await context.params;
-
-//   const backendUrl = process.env.BACKEND_API_URL;
-
-//   const res = await fetch(`${backendUrl}/api/item/${id}`);
-
-//   const data = await res.json();
-
-//   return Response.json(data, {
-//     status: res.status,
-//   });
-// }
 
 export async function GET(req: NextRequest) {
   const backendUrl = process.env.BACKEND_API_URL;
