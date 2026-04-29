a9cd7b75df57d6dd0acbd00726a8797dbd3171af
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-04-29 11:52:39 +0700

    fix useSearchParams

--- app/(shop)/page.tsx
+++ app/(shop)/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-
+export const dynamic = "force-dynamic";
 export default function RootPage() {
   // Tự động chuyển hướng sang trang sản phẩm
   redirect("/products");
