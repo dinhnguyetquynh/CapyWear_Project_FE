94e6862182f0b9f8125d5c231efd2e0c896a0e3e
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-04-30 14:21:32 +0700

    change product page to render dynamic

--- app/(shop)/products/page.tsx
+++ app/(shop)/products/page.tsx
@@ -1,7 +1,7 @@
 import { ProductListParams } from "@/types/params";
 import ProductSection from "./product-section";
 import { Suspense } from "react";
-
+export const dynamic = "force-dynamic";
 export default async function HomePage({ searchParams }:{searchParams:Promise<ProductListParams>}) {
   const params = await searchParams;
   return (
