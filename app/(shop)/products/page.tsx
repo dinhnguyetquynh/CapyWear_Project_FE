1ef82b2a39281a4094c553f29b216768743d25f5
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-04-30 14:19:01 +0700

    wrap suspense for ProductSection

--- app/(shop)/products/page.tsx
+++ app/(shop)/products/page.tsx
@@ -1,11 +1,14 @@
 import { ProductListParams } from "@/types/params";
 import ProductSection from "./product-section";
+import { Suspense } from "react";
 
 export default async function HomePage({ searchParams }:{searchParams:Promise<ProductListParams>}) {
   const params = await searchParams;
   return (
     <div className="max-w-7xl min-h-screen">
-      <ProductSection params={params} />
+      <Suspense fallback={<div>Đang tải...</div>}>
+        <ProductSection params={params} />
+      </Suspense>
     </div>
   )
 }
