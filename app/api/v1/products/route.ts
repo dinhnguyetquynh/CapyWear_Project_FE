76eb6099cede90794d02a65447bb21e783c09e32
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-05-05 09:16:42 +0700

    fix undefined in api-client

--- app/api/v1/products/route.ts
+++ app/api/v1/products/route.ts
@@ -1,16 +1,33 @@
 import { ItemRes, PageResponse } from "@/types/item";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request : NextRequest):Promise<NextResponse>{
-    const { searchParams } = new URL(request.url)
-    const minPrice = searchParams.get('minPrice') ?? '0'
-    const maxPrice = searchParams.get('maxPrice') ?? '0'
+export async function GET(request: NextRequest): Promise<NextResponse> {
+  const { searchParams } = new URL(request.url);
 
-    try{
-        const res = await fetch(`${process.env.BACKEND_API_URL}/api/item/range-price?minPrice=${minPrice}&maxPrice=${maxPrice}`);
-        const data:PageResponse<ItemRes> = await res.json()
-        return NextResponse.json(data)
-    }catch (error) {
-    return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 })
+  const minPrice = searchParams.get('minPrice');
+  const maxPrice = searchParams.get('maxPrice');
+
+  const query = new URLSearchParams();
+
+  if (minPrice !== null && minPrice !== "") {
+    query.append('minPrice', minPrice);
+  }
+
+  if (maxPrice !== null && maxPrice !== "") {
+    query.append('maxPrice', maxPrice);
+  }
+
+  try {
+    const res = await fetch(
+      `${process.env.BACKEND_API_URL}/api/item/range-price?${query.toString()}`
+    );
+
+    const data: PageResponse<ItemRes> = await res.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Failed to fetch products' },
+      { status: 500 }
+    );
   }
 }
