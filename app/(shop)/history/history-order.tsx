5079e9c37abea76298b18a33596f5c5a6d6a333e
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-05-13 10:43:23 +0700

    add status for history order

--- app/(shop)/history/history-order.tsx
+++ app/(shop)/history/history-order.tsx
@@ -8,29 +8,48 @@ export default function HistoryOrder({ orders }: { orders: OrderRespone[] }){
         <div>
            <div>
                 <div className="mb-4 text-center font-bold">LỊCH SỬ ĐƠN HÀNG</div>
-                {orders.map((order)=>(
-                <div key={order.orderId} className="border border-gray-300 p-4 rounded-2xl mb-4 pb-2">
-                    <div>Mã Order: {order.orderId}</div>
-                    <div>Ngày Order: {order.orderDate}</div>
-                    <div>
-                        Trạng thái: 
-                    <span className=" text-red-500">
-                        {
-                            order.status =='PENDING'? 'Đang giao hàng':'Đã giao hàng'
-                        }
-                    </span>
+                  {orders.length === 0 ? (
+                    <div className="text-center text-gray-500 mt-10">
+                        Bạn chưa có đơn hàng nào
                     </div>
-                    {order.details.map((item)=>(
-                        <OrderItem
-                            key={item.id} 
-                            {...item}
-                        />
+                ) : (
+                    orders.map((order) => (
+                        <div
+                            key={order.orderId}
+                            className="border border-gray-300 p-4 rounded-2xl mb-4 pb-2"
+                        >
+                            <div>Mã Order: {order.orderId}</div>
+                            <div>Ngày Order: {order.orderDate}</div>
+
+                            <div>
+                                Trạng thái:
+                                <span className="text-red-500 ml-1">
+                                    {
+                                        order.status == 'PENDING'
+                                            ? 'Đang giao hàng'
+                                            : 'Đã giao hàng'
+                                    }
+                                </span>
+                            </div>
+
+                            {order.details.map((item) => (
+                                <OrderItem
+                                    key={item.id}
+                                    {...item}
+                                />
+                            ))}
+
+                            <div className="text-right text-lg">
+                                Tổng tiền :
+                                <span className="text-red-500">
+                                    {" "}
+                                    {order.totalOrder.toLocaleString("vi")} VND
+                                </span>
+                            </div>
+                        </div>
                     ))
-                    }
-                    <div className="text-right text-lg">Tổng tiền : <span className=" text-red-500">{order.totalOrder.toLocaleString("vi")} VND</span></div>
-                </div>
-                    
-                ))}
+                )}
+
            </div>
         </div>
     )
