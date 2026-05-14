beec9c6cb23bb2d7e6c744800a4a22de8739b6bf
Quynh Dinh Nguyet <dquynhd202@gmail.com>
2026-05-14 11:03:14 +0700

    fix handleIncrement item in ItemDetailPage

--- app/(shop)/item-details/[id]/item-detail.tsx
+++ app/(shop)/item-details/[id]/item-detail.tsx
@@ -22,9 +22,9 @@ export default  function ItemDetail({item}:Props){
     const { fetchCartCount } = useCart();
 
     const handleIncrement =()=>{
-        if(count<=item.inventoryQty){
-            setCount(prev=>prev+1);
-        }
+       if (count < item.inventoryQty) {
+        setCount(prev => prev + 1);
+       }
     }
     const handleDecrement =()=>{
         if(count>1){
